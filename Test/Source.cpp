7bbfcc3893c3d3630ea476781ac60cf7505bc97a
CCF <CCF@CCF12>
2019-01-24 16:15:50 +0100

    m^p:

--- Test/Source.cpp
+++ Test/Source.cpp
@@ -1,5 +1,6 @@
 #include "dl_dxf.h"
 #include "filtreDxf.h"
+#include "roro.cpp"
 
 using namespace std;
 
@@ -24,19 +25,30 @@ int main() {
 	
 
 	int a;
-	if (dxf.in(("C:\\Users\\asus\\Desktop\\SauvAlexis\\source\\repos\\Test\\ellipse.dxf"), &Filtre)) {
+	if (dxf.in(("C:\\Users\\CCF\\source\\repos\\BlackDesire54190\\ProjetCharlyBot1\\polyligne.dxf"), &Filtre)) {
 		search();
 		vector<float>Coordonnee = Filtre.Coordonnee;
 		vector<int>CoordAbs = Filtre.CoordAbs;
 		vector<string>Texte = Filtre.Texte;
-		for (int i = 0; i < Coordonnee.size(); i++) {
+
+
+		/*for (int i = 0; i < Coordonnee.size(); i++) {
 			cout <<Texte[i]<<":" <<Coordonnee[i]<<endl;
-		}
+		}*/
+
+
+
+		test();
+
+
+
+
 		cin >> a;
 	}
 	else {
 		
 		cout << "Le Fihcier n'existe pas" << endl;
+		
 		cin >> a;
 	}
 	return 0;
---
+++ Test/roro.cpp
@@ -0,0 +1,10 @@
+#include "dl_dxf.h"
+#include "filtreDxf.h"
+
+using namespace std;
+
+void test() {
+	for (int i = 0; i < Coordonnee.size(); i++) {
+		cout << Texte[i] << ":" << Coordonnee[i] << endl;
+	}
+}
